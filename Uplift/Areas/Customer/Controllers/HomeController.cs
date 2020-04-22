6d1586ad988a0e9a455a22fb7010c4a1e9a5928f
Jacob Waggoner <jewaggoner@crimson.ua.edu>
2020-04-21 21:55:47 -0400

    Added Azure OCR function

--- Uplift/Areas/Customer/Controllers/HomeController.cs
+++ Uplift/Areas/Customer/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace Uplift.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Upload()
         {
             return View();
         }
--- Uplift/wwwroot/js/site.js
+++ Uplift/wwwroot/js/site.js
@@ -2,3 +2,37 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function submitComment(commentControl) {
+    var comments = document.getElementsByName(commentControl)[0].value;
+    var subscriptionKey = "TEXT_ANALYTICS_API_KEY";
+    var url = "TEXT_ANALYTICS_ENDPOINT/text/analytics/v2.1/sentiment";
+
+    var payload = '{ "documents": [ { "language": "en-US", "id": "1", "text": "' + comments + '" }]}';
+
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: payload,
+        processData: false,
+        headers: {
+            "Ocp-Apim-Subscription-Key": subscriptionKey,
+            "Content-Type": "application/json"
+        }
+    }).done(function (data) {
+
+        var sentimentRating = Math.round((data.documents[0].score * 100) / 25) + 1;
+
+        var currentItemId = JSON.parse(localStorage.getItem('currentItemId'));
+
+        window.location.href = '/comments?classId=' + currentItemId + '&rating=' + sentimentRating + '&comments=' + comments;
+
+    }).fail(function (xhr, status, err) {
+        alert(err);
+    });
+}
+
+function navigateToComments(classId) {
+    localStorage.setItem('currentItemId', JSON.stringify(classId));
+    window.location.href = 'comments?classId=' + classId;
+}
